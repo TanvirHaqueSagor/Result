ac9c85e4e8e3c193d1379f1373a07ca1c5d584d6
TanvirHaqueSagor <tanvirhaquesagor@gmail.com>
2017-11-14 04:47:33 +0600

    first

---
+++ Result/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Result.Startup))]
+namespace Result
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
